545b7511ad14e696d799d5d1453be8a66974068a
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2013-12-09 07:02:30 +0000

    Use full paths

--- fetch_rates.sh
+++ fetch_rates.sh
@@ -1,3 +1,3 @@
-curl https://api.bitcoinaverage.com/exchanges/CAD > cad.json
-curl https://api.bitcoinaverage.com/exchanges/USD > usd.json
-curl "http://rate-exchange.appspot.com/currency?from=USD&to=CAD" > usd_cad.json 
+curl https://api.bitcoinaverage.com/exchanges/CAD > /var/www/bitcoincoop.org/cad.json
+curl https://api.bitcoinaverage.com/exchanges/USD > /var/www/bitcoincoop.org/usd.json
+curl "http://rate-exchange.appspot.com/currency?from=USD&to=CAD" > /var/www/bitcoincoop.org/usd_cad.json 
